9e0ed8bdfcdaa8c7a57839434b0a08c9626a5d1e
William Linna <william.linna@kapsi.fi>
2014-12-20 01:08:00 +0200

    home-routes now serves static file index.html

--- src/mafia_votecount/routes/home.clj
+++ src/mafia_votecount/routes/home.clj
@@ -1,9 +1,10 @@
 (ns mafia-votecount.routes.home
+  (:use ring.util.response)
   (:require [compojure.core :refer :all]
             [mafia-votecount.views.layout :as layout]))
 
-(defn home []
-  (layout/common [:h1 "Hello World!"]))
+;; (defn home []
+;;   (layout/common [:h1 "Hello World!"]))
 
 (defroutes home-routes
-  (GET "/" [] (home)))
+  (GET "/" [] (resource-response "index.html" {:root "public"})))
