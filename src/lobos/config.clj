098313160df6d87018b32d634df95ab142bd03bc
William Linna <william.linna@openmailbox.org>
2015-05-18 11:38:31 +0300

    Global connection is now opened only if needed

--- src/lobos/config.clj
+++ src/lobos/config.clj
@@ -1,9 +1,26 @@
 (ns lobos.config
   (:use lobos.connectivity))
 
+
+;; http://pupeno.com/2011/08/20/how-to-use-lobos-with-heroku/
+(defn open-global-when-necessary
+  "Open a global connection only when necessary, that is, when no previous
+  connection exist or when db-spec is different to the current global
+  connection."
+  [db-spec]
+  ;; If the connection credentials has changed, close the connection.
+  (when (and (@lobos.connectivity/global-connections :default-connection)
+             (not= (:db-spec (@lobos.connectivity/global-connections :default-connection)) db-spec))
+    (lobos.connectivity/close-global))
+  ;; Open a new connection or return the existing one.
+  (if (nil? (@lobos.connectivity/global-connections :default-connection))
+    ((lobos.connectivity/open-global db-spec) :default-connection)
+    (@lobos.connectivity/global-connections :default-connection)))
+
+
 (def h2
   {:classname   "org.h2.Driver"
    :subprotocol "h2"
    :subname     "./resources/database"})
 
-(open-global h2)
+(open-global-when-necessary h2)
