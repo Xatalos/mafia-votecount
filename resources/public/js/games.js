a443d33228014d6d7e4737c1e15cb514df53f420
Teemu Salminen <teemujsalminen@gmail.com>
2015-07-18 22:56:07 +0300

    fixed hopefully

--- resources/public/js/games.js
+++ resources/public/js/games.js
@@ -181,7 +181,7 @@ function showGame(id) {
                 gameToHtml += '<br><textarea id="reply_area" cols=80 rows=25 name="bericht" style="font:10pt Arial; margin-bottom: 4px; width:732px">';
                 currentDay = gameData.votes[gameData.votes.length - 1].day;
             
-                gameToHtml += "[blue][b][u][big]Day " + currentDay + " Votecount[/big][/u][/b][/blue]" & vbCrLf;
+                gameToHtml += "[blue][b][u][big]Day " + currentDay + " Votecount[/big][/u][/b][/blue]\r\n";
         
                 var targets = [];
             
@@ -223,7 +223,7 @@ function showGame(id) {
                             voterscount++;
                         }
                     }
-                    gameToHtml += "[b]" + targets[i] + " (" + voterscount + "):[/b] " + voters.join(', ') & vbCrLf;
+                    gameToHtml += "[b]" + targets[i] + " (" + voterscount + "):[/b] " + voters.join(', ') + "\r\n";
             }
                 gameToHtml += '</textarea>';
         }
