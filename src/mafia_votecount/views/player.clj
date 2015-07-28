43e4df1702df3de6fc44984e52ff98f39e530057
Teemu Salminen <teemujsalminen@gmail.com>
2015-07-28 21:48:35 +0300

    fourth attempt

--- src/mafia_votecount/views/player.clj
+++ src/mafia_votecount/views/player.clj
@@ -2,5 +2,5 @@
   (:require [mafia-votecount.models :as models]
             [liberator.core :refer [defresource resource request-method-in]]))
 
-(defn- delete-player [id]
+(defn delete-player [id]
   (models/delete-player-by-id id))
