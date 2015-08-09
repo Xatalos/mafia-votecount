495a2454ba05cb5646526f01806bb11c2909b63a
Teemu Salminen <teemujsalminen@gmail.com>
2015-08-09 12:17:22 +0300

    added another missing toLowerCase()

--- resources/public/js/games.js
+++ resources/public/js/games.js
@@ -239,7 +239,7 @@ function showGame(id) {
                                 }
                             }
                         }
-                        if (currentDay == gameData.votes[j].day && gameData.votes[j].target.toLowerCase() == targets[i] && gameData.votes[j].target != "") {
+                        if (currentDay == gameData.votes[j].day && gameData.votes[j].target.toLowerCase() == targets[i].toLowerCase() && gameData.votes[j].target != "") {
                             // add a new voter to an existing vote target
                             voters.push(gameData.votes[j].voter);
                         }
