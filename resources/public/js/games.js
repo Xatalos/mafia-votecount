25a8c97485d81c94dd828d4e4944d435aa815d12
Teemu Salminen <teemujsalminen@gmail.com>
2015-08-12 22:21:03 +0300

    fixed the targets capitalization more

--- resources/public/js/games.js
+++ resources/public/js/games.js
@@ -172,11 +172,15 @@ function showGame(id) {
                                     // if a vote target equafls one of the nicknames, then change the vote to target the "real" name
                                     gameData.votes[i].target = key;
                                 }
+                                if (nicknameslist[j].toLowerCase() == gameData.votes[i].target.toLowerCase()) {
+                                    // if a vote target equals a name with the wrong capitalization, then change the vote to target the "real" name                                     // (if it's in the nicknames list)
+                                    gameData.votes[i].target = players[j];
+                                }
                             }
                         }
                         for (var j = 0; j < players.length; j++) {
                             if (players[j].toLowerCase() == gameData.votes[i].target.toLowerCase()) {
-                                // if a vote target equals a name with the wrong capitalization, then change the vote to target the "real" name
+                                // if a vote target equals a name with the wrong capitalization, then change the vote to target the "real" name                                     // (if it's in the players list)
                                 gameData.votes[i].target = players[j];
                             }
                         }
