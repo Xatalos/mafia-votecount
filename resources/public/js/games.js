e309a2ec7bcd173dff9ac97c9e2b5728619a390b
Teemu Salminen <teemujsalminen@gmail.com>
2015-08-13 23:16:41 +0300

    moved nicknames to a separate file

--- resources/public/js/games.js
+++ resources/public/js/games.js
@@ -110,48 +110,7 @@ function showGame(id) {
             
             gameToHtml += "<br>Click on a player to permanently remove him from the game! (reload the page to see the result)<br>";
             
-            var nicknames = {
-                "27ninjabunnies": ["27nb", "nb", "ninja", "bunnies", "ninjabunnies"],
-                "Alakaslam": ["slam"],
-                "Artanis[Xp]": ["art", "artanis"],
-                "batsnacks": ["bat", "bats"],
-                "Bill Murray": ["bm", "bill", "murray"],
-                "Blazinghand": ["bh", "blazing"],
-                "boxerfred": ["bf", "boxer"],
-                "Chezinu": ["che", "chez"],
-                "Clarity_nl": ["clarity"],
-                "Damdred": ["damd", "damdy"],
-                "EdgarAllanPoE": ["edgar", "allan", "poe", "edgarallenpoe"],
-                "Fecalfeast": ["ff", "fecal"],
-                "Fidei86": ["fidei"],
-                "FirmTofu": ["ft", "firm", "tofu"],
-                "GlowingBear": ["gb", "glowing", "bear"],
-                "Half the Sky": ["hts", "sky"],
-                "Hapahauli": ["hapa"],
-                "Holyflare": ["hf"],
-                "IAmRobik": ["robik"],
-                "JudgeJudy": ["jj", "judge", "judy"],
-                "justanothertownie": ["jat"],
-                "Keirathi": ["kei", "keir"],
-                "LightningStrike": ["ls", "lightning"],
-                "marvellosity": ["marv", "marvy", "marvel", "marvelocity", "marshmallow"],
-                "Oatsmaster": ["oat", "oats"],
-                "ObiWanShinobi": ["ows", "obi", "obiwan", "shinobi"],
-                "Onegu": ["o", "oneg"],
-                "raynpelikoneet": ["ray", "rayn", "raypelikoneet"],
-                "rsoultin": ["rs", "rsou", "rsoul"],
-                "sandroba": ["sand", "roba"],
-                "[UoN]Sentinel": ["sent", "sentinel"],
-                "scott31337": ["scott"],
-                "Stutters695": ["stutters"],
-                "The Shining": ["shining"],
-                "Tubesock": ["ts", "tube"],
-                "VayneAuthority": ["va", "vayne"],
-                "VisceraEyes": ["ve", "viscera"],
-                "WaveofShadow": ["wos", "wave"],
-                "Xatalos": ["xat", "xata"],
-                "yamato77": ["yam", "yama", "yamato"]
-            };
+            var nicknames = getNicknames();
 
             var currentDay = 0;
             
---
+++ resources/public/js/nicknames.js
@@ -0,0 +1,48 @@
+function getNicknames() {
+    "use strict";
+    
+    var nicknames = {
+                "27ninjabunnies": ["27nb", "nb", "ninja", "bunnies", "ninjabunnies"],
+                "Alakaslam": ["slam"],
+                "Artanis[Xp]": ["art", "artanis"],
+                "batsnacks": ["bat", "bats"],
+                "Bill Murray": ["bm", "bill", "murray"],
+                "Blazinghand": ["bh", "blazing"],
+                "boxerfred": ["bf", "boxer"],
+                "Chezinu": ["che", "chez"],
+                "Clarity_nl": ["clarity"],
+                "Damdred": ["damd", "damdy"],
+                "EdgarAllanPoE": ["edgar", "allan", "poe", "edgarallenpoe"],
+                "Fecalfeast": ["ff", "fecal"],
+                "Fidei86": ["fidei"],
+                "FirmTofu": ["ft", "firm", "tofu"],
+                "GlowingBear": ["gb", "glowing", "bear"],
+                "Half the Sky": ["hts", "sky"],
+                "Hapahauli": ["hapa"],
+                "Holyflare": ["hf"],
+                "IAmRobik": ["robik"],
+                "JudgeJudy": ["jj", "judge", "judy"],
+                "justanothertownie": ["jat"],
+                "Keirathi": ["kei", "keir"],
+                "LightningStrike": ["ls", "lightning"],
+                "marvellosity": ["marv", "marvy", "marvel", "marvelocity", "marshmallow"],
+                "Oatsmaster": ["oat", "oats"],
+                "ObiWanShinobi": ["ows", "obi", "obiwan", "shinobi"],
+                "Onegu": ["o", "oneg"],
+                "raynpelikoneet": ["ray", "rayn", "raypelikoneet"],
+                "rsoultin": ["rs", "rsou", "rsoul"],
+                "sandroba": ["sand", "roba"],
+                "[UoN]Sentinel": ["sent", "sentinel"],
+                "scott31337": ["scott"],
+                "Stutters695": ["stutters"],
+                "The Shining": ["shining"],
+                "Tubesock": ["ts", "tube"],
+                "VayneAuthority": ["va", "vayne"],
+                "VisceraEyes": ["ve", "viscera"],
+                "WaveofShadow": ["wos", "wave"],
+                "Xatalos": ["xat", "xata"],
+                "yamato77": ["yam", "yama", "yamato"]
+            };
+    
+    return nicknames;
+}
